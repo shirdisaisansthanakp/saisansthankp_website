6ea6260f74c06da46f7272948e23830eb8c0dc59
Ganapa Sai Manoj <ganapasaimanoj@gmail.com>
2022-08-11 15:37:52 +0530

    initial

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,14 @@
+import Image from "next/image";
+
 export default function Header() {
     return (
-        <header>
-            <img />
-
+        <header className="flex content">
             <section>
-                <h1></h1>
-                <p></p>
+                <Image className="logo" src="/images/logo.jpg" height={75} width={75} />
+            </section>
+            <section className="title-section">
+                <h1>shiridi sai sansthan</h1>
+                <p className="subtitle">velipula veeshi, opposite kasi vishweshwara temple, anakapalle.</p>
             </section>
         </header>
     );
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import Header from "../components/Header";
+
 export default function Home() {
   return (
-    <h3>Welcome</h3>
+    <Header />
   )
 }
